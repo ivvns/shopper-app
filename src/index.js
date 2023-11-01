d615db9a58c1a0bdc38e4953b602075ca39a6e86
ivvns <ivansomera@hotmail.com>
2023-10-31 18:24:12 -0700

    adding items to the DOM

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 import '@fortawesome/fontawesome-free/js/all';
 import './styles/style.css';
 
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
+
+function addItem(e) {
+    e.preventDefault();
+
+    const newItem = itemInput.value;
+    // Validate Input
+    if(newItem === '') {
+        alert('Please add an item');
+        return;
+    }   
+
+    // Create list item
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(newItem));
+
+    const button = createButton('remove-item item-btn');
+    li.appendChild(button);
+
+    itemList.appendChild(li);
+
+    itemInput.value = '';
+ }
+
+// Create button
+function createButton(classes) {
+    const button = document.createElement('button');
+    button.className = classes;
+    const icon = createIcon('fa-solid fa-x')
+    button.appendChild(icon);
+    return button;
+ }
+
+// Create icon
+function createIcon(classes) {
+    const icon = document.createElement('i');
+    icon.className = classes;
+    return icon;
+}
+
+// Event Listeners
+itemForm.addEventListener('submit', addItem);
